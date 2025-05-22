d9c7ea505229ea131c97111020228cdf6fce5bda
CHUCHUCHUCHU <167416194+CHU1PC@users.noreply.github.com>
2025-05-22 14:26:24 +0900

    Delete chap2/tempCodeRunnerFile.py

--- chap2/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-# class Exp(Function):
-#     def forward(self, x):
-#         y = np.exp(x)
-#         return y
-
-#     def backward(self, gy):
-#         x = self.input.data
-#         gx = np.exp(x) * gy
-#         return gx
