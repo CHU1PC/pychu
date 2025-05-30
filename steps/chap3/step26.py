83d468fd9ec56b1f28b006d345cc62b88a435c92
CHUCHUCHUCHU <raikyu70@gmail.com>
2025-05-30 18:16:13 +0900

    made: chu1pc

--- steps/chap3/step26.py
+++ steps/chap3/step26.py
@@ -1,6 +1,7 @@
 import sys
 
-sys.path.append(r"D:\program\programming\study\ゼロから作るdeeplearning\ゼロから作る3\from_zero_3_github")
+sys.path.append(r"D:\program\programming\study\ゼロから作るdeeplearning"
+                r"\ゼロから作る3\from_zero_3_github")
 
 import numpy as np  # noqa
 from pychu import Variable  # noqa
