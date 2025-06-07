9dea91f77951f84ac1e728f13885d0f9bf0049ac
CHUCHUCHUCHU <raikyu70@gmail.com>
2025-06-07 21:26:17 +0900

    add sum_to: chu1pc

--- pychu/core.py
+++ pychu/core.py
@@ -272,19 +272,32 @@ class Neg(Function):
 
 class Add(Function):
     def forward(self, x0, x1):
+        self.x0_shape, self.x1_shape = x0.shape, x1.shape
         y = x0 + x1
         return y
 
     def backward(self, gy):
-        return gy, gy
+        gx0, gx1 = gy, gy
+        if self.x0_shape != self.x1_shape:
+            import pychu.functions as F
+            gx0 = F.sum_to(gx0, self.x0_shape)
+            gx1 = F.sum_to(gx1, self.x1_shape)
+        return gx0, gx1
 
 
 class Sub(Function):
     def forward(self, x0, x1):
+        self.x0_shape, self.x1_shape = x0.shape, x1.shape
         return x0 - x1
 
     def backward(self, gy):
-        return gy, -gy
+        gx0 = gy
+        gx1 = -gy
+        if self.x0_shape != self.x1_shape:
+            import pychu.functions as F
+            gx0 = F.sum_to(gx0, self.x0_shape)
+            gx1 = F.sum_to(gx1, self.x1_shape)
+        return gx0, gx1
 
 
 class Mul(Function):
@@ -294,7 +307,13 @@ class Mul(Function):
 
     def backward(self, gy):
         x0, x1 = self.inputs
-        return gy * x1, gy * x0
+        gx0 = gy * x1
+        gx1 = gy * x0
+        if x0.shape != x1.shape:
+            import pychu.functions as F
+            gx0 = F.sum_to(gx0, x0.shape)
+            gx1 = F.sum_to(gx1, x1.shape)
+        return gx0, gx1
 
 
 class Div(Function):
@@ -303,7 +322,13 @@ class Div(Function):
 
     def backward(self, gy):
         x0, x1 = self.inputs
-        return gy * (1 / x1), gy * (-x0 / x1 ** 2)
+        gx0 = gy / x1
+        gx1 = gy * (-x0 / x1 ** 2)
+        if x0.shape != x1.shape:
+            import pychu.functions as F
+            gx0 = F.sum_to(gx0, x0.shape)
+            gx1 = F.sum_to(gx1, x1.shape)
+        return gx0, gx1
 
 
 class Pow(Function):
