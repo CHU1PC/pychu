8051c20cf635db9f07b1ef944d3bbcf06e41971c
CHUCHUCHUCHU <raikyu70@gmail.com>
2025-06-09 12:02:59 +0900

    made: chu1pc

---
+++ pychu/layers.py
@@ -0,0 +1,93 @@
+import sys
+import weakref
+
+import numpy as np
+
+sys.path.append(r"D:\program\programming\study\ゼロから作るdeeplearning"
+                r"\ゼロから作る3\from_zero_3_github")
+
+import pychu.functions as F  # noqa
+from pychu.core import Parameter  # noqa
+
+"""
+パラメータの自動化を行うためのファイル
+"""
+
+
+class Layer:
+    def __init__(self):
+        """
+        self._paramsにはLayerのインスタンスが持つパラメータを保持するもの
+        """
+        self._params = set()
+
+    def __setattr__(self, name, value):
+        """インスタンス変数を宣言する際に呼び出されるメソッド
+
+        Args:
+            name (str): 名前を保持するための変数
+            value (int, float, double)): 値を保持するための変数
+        """
+        if isinstance(value, (Parameter, Layer)):
+            self._params.add(name)
+        super().__setattr__(name, value)
+
+    def __call__(self, *inputs):
+        outputs = self.forward(*inputs)
+        if not isinstance(outputs, tuple):
+            outputs = (outputs, )
+        self.inputs = [weakref.ref(x) for x in inputs]
+        self.outputs = [weakref.ref(y) for y in outputs]
+        return outputs if len(outputs) > 1 else outputs[0]
+
+    def forward(self, inputs):
+        raise NotImplementedError
+
+    def params(self):
+        """
+        もしobjがLayerならLayerのなかのパラメータを取り出し
+        もしobjがParameterならばそのまま返す
+        """
+        for name in self._params:
+            obj = self.__dict__[name]
+
+            if isinstance(obj, Layer):
+                # obj.params()のなかのparamsをyieldで取り出す
+                yield from obj.params()
+            else:
+                yield obj
+
+    def cleargrads(self):
+        for param in self.params():
+            param.cleargrad()
+
+
+class Linear(Layer):
+    def __init__(self, out_size, nobias=False, dtype=np.float32, in_size=None):
+        super().__init__()
+        self.in_size = in_size
+        self.out_size = out_size
+        self.dtype = dtype
+
+        self.W = Parameter(None, name="W")
+        if self.in_size is not None:
+            self._init_W()
+
+        if nobias:
+            self.b = None
+        else:
+            self.b = Parameter(np.zeros(out_size, dtype=dtype), name="b")
+
+    def _init_W(self):
+        In, Out = self.in_size, self.out_size
+        W_data = np.random.randn(In, Out).astype(self.dtype) * np.sqrt(1 / In)
+        self.W.data = W_data
+
+    def forward(self, x):
+        # in_sizeがNoneでself.WがNoneで初期化されていた場合forwardが呼び出されるときに入力と同じサイズだけ作る
+        if self.W.data is None:
+            self.in_size = x.shape[1]
+            self._init_W()
+
+        y = F.linear(x, self.W, self.b)
+        return y
---
+++ pychu/models.py
@@ -0,0 +1,31 @@
+import sys
+
+sys.path.append(r"D:\program\programming\study\ゼロから作るdeeplearning"
+                r"\ゼロから作る3\from_zero_3_github")
+import pychu.functions as F  # noqa
+import pychu.layers as L  # noqa
+from pychu import Layer  # noqa
+from pychu import utils  # noqa
+
+
+class Model(Layer):
+    def plot(self, *inputs, to_file="model.png"):
+        y = self.forward(*inputs)
+        return utils.plot_dot_graph(y, verbose=True, to_file=to_file)
+
+
+class MLP(Model):
+    def __init__(self, fc_output_sizes, activation=F.sigmoid):
+        super().__init__()
+        self.activation = activation
+        self.layers = []
+
+        for i, out_size in enumerate(fc_output_sizes):
+            layer = L.Linear(out_size)
+            setattr(self, "l" + str(i), layer)
+            self.layers.append(layer)
+
+    def forward(self, x):
+        for layer in self.layers[:-1]:
+            x = self.activation(layer(x))
+        return self.layers[-1](x)
