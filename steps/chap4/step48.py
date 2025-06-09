bc193648b4fdc3b04df95ffcb9e87037e359a354
CHUCHUCHUCHU <raikyu70@gmail.com>
2025-06-09 16:11:54 +0900

    made: chu1pc

---
+++ steps/chap4/step48.py
@@ -0,0 +1,18 @@
+import math
+import sys
+
+import numpy as np
+
+sys.path.append(r"D:\program\programming\study\ゼロから作るdeeplearning"
+                r"\ゼロから作る3\from_zero_3_github")
+
+import pychu
+import pychu.functions as F
+from pychu import optimizer
+from pychu.models import MLP
+
+max_epoch = 300
+batch_size= 30
+hidden_size = 10
+lr = 1.0
+x, t = pychu.data
