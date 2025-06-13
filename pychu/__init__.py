7d4b9491a3dd707f88a764a637cc9433339af564
CHUCHUCHUCHU <raikyu70@gmail.com>
2025-06-14 00:07:42 +0900

    change pattern: chu1pc

--- pychu/__init__.py
+++ pychu/__init__.py
@@ -5,14 +5,27 @@ if is_simple_core:
     from pychu.core_simple import (Function, Variable, as_variable,  # noqa
                                    no_grad)
 else:
-    from pychu.core import using_config  # noqa
-    from pychu.core import (Function, Parameter,  # type: ignore  # noqa
-                            Variable, _setup_variable_operators, as_variable,
-                            no_grad)
-    from pychu.dataloader import DataLoader  # noqa
-    from pychu.layers import Layer  # noqa
-    from pychu.models import Model  # noqa
-    import pychu.transforms  # noqa
-    import pychu.optimizers  # noqa
+    from pychu.core import Variable  # type: ignore # noqa
+    from pychu.core import Parameter  # type: ignore # noqa
+    from pychu.core import Function  # type: ignore # noqa
+    from pychu.core import using_config  # type: ignore # noqa
+    from pychu.core import no_grad  # type: ignore # noqa
+    from pychu.core import as_array  # type: ignore # noqa
+    from pychu.core import as_variable  # type: ignore # noqa
+    from pychu.core import setup_variable  # type: ignore # noqa
+    from pychu.core import Config  # type: ignore # noqa
+    from pychu.layers import Layer  # type: ignore # noqa
+    from pychu.models import Model  # type: ignore # noqa
+    from pychu.datasets import Dataset  # type: ignore # noqa
+    from pychu.dataloader import DataLoader  # type: ignore # noqa
 
-_setup_variable_operators()
+    import pychu.datasets  # type: ignore # noqa
+    import pychu.dataloader  # type: ignore # noqa
+    import pychu.optimizers  # type: ignore # noqa
+    import pychu.functions  # type: ignore # noqa
+    import pychu.layers  # type: ignore # noqa
+    import pychu.utils  # type: ignore # noqa
+    import pychu.cuda  # type: ignore # noqa
+    import pychu.transforms  # type: ignore # noqa
+
+setup_variable()
