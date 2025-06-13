60c12b8c4e8273e9c6855f31d7697f2251700d28
CHUCHUCHUCHU <raikyu70@gmail.com>
2025-06-13 17:14:42 +0900

    add comment: chu1pc

--- steps/chap4/step51.py
+++ steps/chap4/step51.py
@@ -1,13 +1,13 @@
-import sys
 import os
-sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
-import pychu
-from pychu import DataLoader
-import pychu.datasets as datasets
-from pychu.models import MLP
-import pychu.optimizers as optimizers
-import pychu.functions as F
+import sys
 
+sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
+import pychu  # noqa
+import pychu.datasets as datasets  # noqa
+import pychu.functions as F  # noqa
+import pychu.optimizers as optimizers  # noqa
+from pychu import DataLoader  # noqa
+from pychu.models import MLP  # noqa
 
 max_epoch = 5
 batch_size = 100
@@ -34,19 +34,19 @@ for epoch in range(max_epoch):
         loss.backward()
         optim.update()
 
-        sum_loss += float(loss.data) * len(t)
-        sum_acc += float(acc.data) * len(t)
+        sum_loss += float(loss.data) * len(t)  # type: ignore
+        sum_acc += float(acc.data) * len(t)  # type: ignore
     print(f"epoch: {(epoch + 1)}")
     print(f"train loss: {(sum_loss / len(train_set)):.4f}, "
           f"accuracy: {(sum_acc / len(train_set)):.4f}")
-    
+
     sum_loss, sum_acc = 0, 0
     with pychu.no_grad():
         for x, t in test_loader:
             y = model(x)
             loss = F.softmax_cross_entropy(y, t)
             acc = F.accuracy(y, t)
-            sum_loss += float(loss.data) * len(t)
-            sum_acc += float(acc.data) * len(t)
+            sum_loss += float(loss.data) * len(t)  # type: ignore
+            sum_acc += float(acc.data) * len(t)  # type: ignore
     print(f"test loss: {(sum_loss / len(test_set)):.4f}, "
           f"tets accuracy: {(sum_acc / len(test_set)):.4f}")
