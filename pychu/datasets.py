b7919d55640a83079b36865dc9a6305788664886
CHUCHUCHUCHU <raikyu70@gmail.com>
2025-06-20 18:16:56 +0900

    add Shakespear dataset: chu1pc

--- pychu/datasets.py
+++ pychu/datasets.py
@@ -182,3 +182,29 @@ class SinCurve(Dataset):
         y = y.astype(dtype)
         self.data = y[:-1][:, np.newaxis]
         self.label = y[1:][:, np.newaxis]
+
+
+class Shakespear(Dataset):
+
+    def prepare(self):
+        url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master'\
+            '/data/tinyshakespeare/input.txt'
+        file_name = 'shakespear.txt'
+        path = get_file(url, file_name)
+        with open(path, 'r') as f:
+            data = f.read()
+        chars = list(data)
+
+        char_to_id = {}  # type: ignore
+        id_to_char = {}
+        for word in data:
+            if word not in char_to_id:
+                new_id = len(char_to_id)
+                char_to_id[word] = new_id
+                id_to_char[new_id] = word
+
+        indices = np.array([char_to_id[c] for c in chars])
+        self.data = indices[:-1]
+        self.label = indices[1:]
+        self.char_to_id = char_to_id
+        self.id_to_char = id_to_char
