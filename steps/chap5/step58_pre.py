fda9038e3dfa3a4852e57cfae5b2b7481bc17b7a
CHUCHUCHUCHU <raikyu70@gmail.com>
2025-06-20 15:12:37 +0900

    change how to eval: chu1pc

--- steps/chap5/step58_pre.py
+++ steps/chap5/step58_pre.py
@@ -6,18 +6,21 @@ from PIL import Image
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 import pychu.datasets  # noqa
 import pychu  # noqa
+import pychu.utils  # noqa
 import pychu.config as config  # noqa
 from pychu.models import VGG16  # noqa
 
-img_path = os.path.join(config.DATA_PATH, "zebra.jpg")
-
+url = "https://github.com/oreilly-japan/deep-learning-from-scratch-3/"\
+    "raw/images/zebra.jpg"
+img_path = pychu.utils.get_file(url)
 img = Image.open(img_path)
+
 x = VGG16.preprocess(img)
 x = x[np.newaxis]
 
 model = VGG16(pretrained=True)
-with pychu.test_mode():
-    y = model(x)
+model.eval()
+y = model(x)
 predict_id = np.argmax(y.data)
 
 model.plot(x, to_file=os.path.join(config.DATA_PATH, "vgg.pdf"))
