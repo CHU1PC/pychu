f51c6859c7c808c2ca905bfe3cb68f6848b74384
CHUCHUCHUCHU <raikyu70@gmail.com>
2025-06-20 17:20:46 +0900

    add imgpath: chu1pc

--- pychu/config.py
+++ pychu/config.py
@@ -4,4 +4,6 @@ DATA_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data")
 
 WEIGHTS_PATH = os.path.join(DATA_PATH, "weights")
 
+IMG_PATH = os.path.join(DATA_PATH, "img")
+
 PYCHU_PATH = os.path.dirname(__file__)
