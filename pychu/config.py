6a00deaa913971614a977187ee6d9309777e1fc4
CHUCHUCHUCHU <raikyu70@gmail.com>
2025-06-20 15:00:10 +0900

    add WEIGHTS_PATH: chu1pc

--- pychu/config.py
+++ pychu/config.py
@@ -2,4 +2,6 @@ import os
 
 DATA_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data")
 
+WEIGHTS_PATH = os.path.join(DATA_PATH, "weights")
+
 PYCHU_PATH = os.path.dirname(__file__)
