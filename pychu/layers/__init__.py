0975376c1bb61254f67cf9afb2605f1f8609f1fd
CHUCHUCHUCHU <raikyu70@gmail.com>
2025-06-23 00:01:29 +0900

    add TimeDropout: chu1pc

--- pychu/layers/__init__.py
+++ pychu/layers/__init__.py
@@ -1,4 +1,4 @@
-from .main_layer import Layer, Dropout  # noqa
+from .main_layer import Layer, Dropout, TimeDropout  # noqa
 
 from .Linear import Linear, TimeLinear  # noqa
 
@@ -7,3 +7,5 @@ from .Convolution import Conv2d  # noqa
 from .RNN import RNN, TimeRNN  # noqa
 
 from .LSTM import LSTM, TimeLSTM  # noqa
+
+from .Embedding import Embedding, TimeEmbedding  # noqa
